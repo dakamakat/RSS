0c0007a33245225f21b4097d201ba3d9a533ff6f
dakamakat <gazon-ozon@yandex.ru>
2024-05-12 20:43:00 +0200

    feat(server): setup defaults

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -20,6 +21,21 @@ func main() {
 	}
 
 	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+    v1Router := chi.NewRouter()
+    v1Router.Get("/health" , handlerReadiness)
+    v1Router.Get("/error" , handlerError)
+
+    router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
